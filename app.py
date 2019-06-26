dc1f34c6213a9626eaefe8445bba07ba626fefdc
Mahi <47994802+Mahisaurus-Rex@users.noreply.github.com>
2019-06-26 11:35:00 -0400

    note: use basic layout from word reader

--- app.py
+++ app.py
@@ -6,7 +6,17 @@ import glob, os
 
 class GUI:
     def __init__(self, master):
+        self.master = root
+        master.title("Comic Splicer")
 
+'''
+        #splice button
+        self.splice_button=Button(self.master,text="Splice!", command==self.splice)
+        self.splice_button.pack()
+ 
+    def splice(self):
+        print("splice")
+  ''' 
 root = Tk()
 root.geometry("925x510") #GUI start size
 my_gui = GUI(root)
