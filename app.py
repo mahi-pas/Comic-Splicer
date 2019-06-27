6c3441f65858ee62d1840f36802117d1bea31a26
Mahi <mahipasarkar@gmail.com>
2019-06-26 20:26:17 -0400

    base structure

--- app.py
+++ app.py
@@ -2,18 +2,8 @@ from tkinter import *
 from tkinter import scrolledtext
 from tkinter import filedialog
 from PIL import ImageTk,Image
-from PIL import ImageGrab
 import glob, os
-import pyperclip
 
-def resize_img():
-    size = 500, 500 #Thumbnail size
-    #makes thumbnail of input.png, which makes it resize correctly for this. input.thumbnail only used for GUI
-    for infile in glob.glob("*.png"):
-        file, ext = os.path.splitext(infile)
-        im = Image.open(infile)
-        im.thumbnail(size)
-        im.save(file + ".thumbnail", "PNG")
 
 class GUI:
     def __init__(self, master):
@@ -35,31 +25,43 @@ class GUI:
             file_path = filedialog.askopenfilename()
             im = Image.open(file_path)
             im.save("input.png","PNG")
-            resize_img()
-            self.input_image=ImageTk.PhotoImage(Image.open("input.thumbnail"))
-            self.image = self.image_view.create_image(0, 0, anchor=NW, image=self.input_image)
+            #self.image = self.image_view.create_image(0, 0, anchor=NW, image=self.input_image)
 
         #Canvas
-        self.input_image=ImageTk.PhotoImage(Image.open("input.thumbnail")) #Image input file
         self.image_view = Canvas(self.left_side, width=500, height=500)
         self.image_view.bind('<Button-1>', click) #Bind click function to canvas
-        self.image = self.image_view.create_image(0, 0, anchor=NW, image=self.input_image)
-        self.master.bind('<Control-v>', self.paste())
+        #self.image = self.image_view.create_image(0, 0, anchor=NW, image=self.input_image)
         self.image_view.pack()
+        #image width and height input
+        Label(self.right_side, text="Width").grid(row=0)
+        Label(self.right_side, text="Height").grid(row=1)
 
-        #image width and height
-        
+        self.width_input = Entry(self.right_side)
+        self.height_input = Entry(self.right_side)
+
+        self.width_input.grid(row=0, column=1)
+        self.height_input.grid(row=1, column=1)
+
+        #change output path button
+        self.dir_button= Button(self.right_side, text="change output directory",command=self.dir_change)
+        self.dir_button.grid(row=2,column=1)
+
         #convert button
-        self.convert_button = Button(self.right_side, text="Convert", command=self.convert)
-        self.convert_button.pack()
+        self.splice_button = Button(self.right_side, text="Splice!", command=self.splice)
+        self.splice_button.grid(row=3, column=1)
 
 
 
     #fuction for convert button
-    def convert(self):
-        print("convert")
+    def splice(self):
+        print("splice")
+    
+    #func for output path button
+    def dir_change(self):
+        print("dir change")
+
 
 root = Tk()
-root.geometry("925x510") #GUI start size
+root.geometry("730x510") #GUI start size
 my_gui = GUI(root)
 root.mainloop()
